6c37351639188b0ae76b598acb910676fbbfd062
Piechu <p.piechowiak@interia.pl>
2019-05-23 13:24:51 +0200

    repairing tests for controller

--- src/test/java/com/pawelpiechowiak/manager/UserControllerTest.java
+++ src/test/java/com/pawelpiechowiak/manager/UserControllerTest.java
@@ -1,23 +1,29 @@
 package com.pawelpiechowiak.manager;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-
-    @Before
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
-    }
+    @MockBean
+    private UserProvider userProvider;
 
     @Test
     public void index() throws Exception {
@@ -26,17 +32,19 @@ public class UserControllerTest {
                 .andExpect(view().name("index"));
     }
 
-//    @Test
-//    public void table() throws Exception{
-//        this.mockMvc.perform(get("/table"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("table"));
-//    }
-//
-//    @Test
-//    public void adjust() throws Exception{
-//        this.mockMvc.perform(get("/adjust"))
-//                .andExpect(status().isOk())
-//                .andExpect(view().name("adjust"));
-//    }
+    @Test
+    public void table() throws Exception {
+        when(userProvider.getUsers()).thenReturn(new UserProvider().getUsers());
+        mockMvc.perform(get("/table"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("table"));
+    }
+
+    @Test
+    public void adjust() throws Exception {
+        when(userProvider.getUsers()).thenReturn(new UserProvider().getUsers());
+        this.mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("adjust"));
+    }
 }
